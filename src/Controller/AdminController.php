727e9347c7217b5e186ec331d28403783bbc5ac2
Marius Ambrazevicius <work.marius.ambrazevicius@gmail.com>
2019-06-12 12:54:40 +0300

    Stuff

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,6 +15,7 @@ use App\Service\ConfigurationManager;
 use App\Service\EmailTemplateManager;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use PlumTreeSystems\FileBundle\Service\GaufretteFileManager;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -144,7 +145,7 @@ class AdminController extends AbstractController
      * @param ConfigurationManager $cm
      * @return Response
      */
-    public function changeContent(Request $request, ConfigurationManager $cm)
+    public function changeContent(Request $request, ConfigurationManager $cm, GaufretteFileManager $fileManager)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -158,9 +159,19 @@ class AdminController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             if ($tempConfiguration->getMainLogo()) {
+                if ($configuration->getMainLogo()) {
+                    $logo = $configuration->getMainLogo();
+                    $configuration->setMainLogo(null);
+                    $fileManager->removeEntity($logo);
+                }
                 $configuration->setMainLogo($tempConfiguration->getMainLogo());
             }
             if ($tempConfiguration->getTermsOfServices()) {
+                if ($configuration->getTermsOfServices()) {
+                    $temp = $configuration->getTermsOfServices();
+                    $configuration->setTermsOfServices(null);
+                    $fileManager->removeEntity($temp);
+                }
                 $configuration->setTermsOfServices($tempConfiguration->getTermsOfServices());
             }
             $em->persist($configuration);
