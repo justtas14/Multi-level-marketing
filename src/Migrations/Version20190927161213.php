6c72fa4b794de22ba351c29103116d60753bd3dd
Marius Ambrazevicius <work.marius.ambrazevicius@gmail.com>
2019-09-27 20:07:55 +0300

    Fixed migrations

--- src/Migrations/Version20190927161213.php
+++ src/Migrations/Version20190927161213.php
@@ -23,26 +23,7 @@ final class Version20190927161213 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE associate ADD unique_user_name VARCHAR(255) NOT NULL');
-        $this->addSql('DROP FUNCTION IF EXISTS alphanum;');
-        $this->addSql("CREATE FUNCTION alphanum( str CHAR(255) charset utf8 ) RETURNS CHAR(255) charset utf8 DETERMINISTIC ".
-            "BEGIN\n".
-            "DECLARE i, len SMALLINT DEFAULT 1;\n".
-            "DECLARE ret CHAR(255) charset utf8 DEFAULT '';\n".
-            "DECLARE c CHAR(1) charset utf8;\n".
-            "SET len = CHAR_LENGTH( str );\n".
-            "REPEAT\n".
-            "BEGIN\n".
-            "SET c = MID( str, i, 1 );\n".
-            "IF c REGEXP '[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]' THEN\n".
-            "SET ret=CONCAT(ret,c);\n".
-            "END IF;\n".
-            "SET i = i + 1;\n".
-            "END;\n".
-            "UNTIL i > len END REPEAT;\n".
-            "RETURN ret;\n".
-            "END;");
-        $this->addSql('UPDATE associate SET unique_user_name=lower(CONCAT(alphanum(full_name), id))');
-        $this->addSql('DROP FUNCTION IF EXISTS alphanum;');
+        $this->addSql('UPDATE associate SET unique_user_name=associate_id');
     }
 
     public function down(Schema $schema) : void
