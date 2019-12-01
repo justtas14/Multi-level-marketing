602addf9685e3f729d686e6fd79feb2248f058df
justas09 <justtas14@gmail.com>
2019-12-01 23:17:05 +0200

    fixed checkboxes profile, fixed loading, fixed pagination bug, end prelaunch doing

---
+++ client/src/store/modules/endprelaunch.js
@@ -0,0 +1,44 @@
+import ApiCalls from '../api/SecurityApi/apiCalls';
+
+const initialState = {
+    formSuccess: false,
+    hasPrelaunchEnded: false,
+    configurationContent: '',
+};
+
+const getters = {
+
+};
+
+const actions = {
+    async home({ commit, rootState }, formData = null) {
+        const apiCallObj = new ApiCalls();
+        const response = await apiCallObj.endPrelaunch(
+            '/api/admin/endprelaunch',
+            formData,
+            rootState.Security.token,
+        );
+        commit('setGeneralInfo', response.data);
+        return response.data;
+    },
+};
+
+const mutations = {
+    setGeneralInfo(state, data) {
+        state.formSuccess = data.formSuccess;
+        state.hasPrelaunchEnded = data.hasPrelaunchEnded;
+        state.configurationContent = data.configurationContent;
+    },
+
+    setFormSuccess(state, flag) {
+        state.formSuccess = flag;
+    },
+};
+
+export default {
+    namespaced: true,
+    state: initialState,
+    getters,
+    actions,
+    mutations,
+};
---
+++ client/src/views/Admin/EndPrelaunch/EndPrelaunch.vue
@@ -0,0 +1,132 @@
+<template>
+  <div
+    class="admin-contentContainer"
+    style="overflow:hidden"
+  >
+    <div class="card">
+      <div class="card-content quillEditorCard">
+        <span class="card-title">End Prelaunch</span>
+        <Success v-if="formSuccess" v-bind:message="'Form Updated'"></Success>
+        <main id="main">
+          <form
+            name="end_prelaunch"
+            method="post"
+          >
+            <div class="input-field">
+              <label style="position: unset!important;">
+                <input
+                  type="checkbox"
+                  id="end_prelaunch_prelaunchEnded"
+                  name="end_prelaunch[prelaunchEnded]"
+                  class="filled-in"
+                  v-model="formData.prelaunchEnded"
+                >
+                <span>End Prelaunch</span>
+              </label>
+            </div>
+            <div class="input-field">
+              <label for="end_prelaunch_landingContent">Landing content</label>
+              <input
+                type="hidden"
+                id="end_prelaunch_landingContent"
+                name="end_prelaunch[landingContent]"
+              >
+                <content-loader v-if="isLoading" :height='225' >
+                        <rect :width='400' :height='225' />
+                </content-loader>
+                <QuillEditor
+                    v-else
+                    v-bind:configurationContent="formData.configurationContent"
+                    v-model="updateConfigurationContent"
+                >
+                </QuillEditor>
+            </div>
+            <div>
+            <button
+                type="button"
+                id="end_prelaunch_Submit"
+                name="end_prelaunch[Submit]"
+                class="waves-effect waves-light btn"
+                :disabled="isLoadingForm"
+                @click="submitForm"
+              >Save
+              </button>
+                <div v-if="isLoadingForm" class="progress">
+                    <div class="indeterminate"></div>
+                </div>
+            </div>
+          </form>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+    mapActions, mapState, mapMutations,
+} from 'vuex';
+import { ContentLoader } from 'vue-content-loader';
+import Success from '../../../components/Messages/Success.vue';
+import QuillEditor from '../../../components/Gallery/QuillEditor.vue';
+
+export default {
+    name: 'EndPrelaunch',
+    components: {
+        Success,
+        QuillEditor,
+        ContentLoader,
+    },
+    props: [],
+    data() {
+        return {
+            isLoading: false,
+            isLoadingForm: false,
+            updateConfigurationContent: '',
+            formData: {
+                prelaunchEnded: false,
+                configurationContent: '',
+            },
+        };
+    },
+    methods: {
+        async submitForm() {
+            const formData = new FormData();
+            formData.append('prelaunchEnded', this.formData.prelaunchEnded);
+            formData.append('landingContent', this.updateConfigurationContent);
+            this.isLoadingForm = true;
+            await this.home(formData);
+            this.isLoadingForm = false;
+        },
+
+        ...mapMutations('EndPrelaunch', [
+            'setFormSuccess',
+        ]),
+        ...mapActions('EndPrelaunch', [
+            'home',
+        ]),
+    },
+    mounted() {
+
+    },
+    computed: {
+
+        ...mapState('EndPrelaunch', [
+            'formSuccess',
+        ]),
+    },
+    async created() {
+        this.setFormSuccess(false);
+        this.isLoading = true;
+        const res = await this.home();
+        console.log(res);
+        this.isLoading = false;
+        this.formData.prelaunchEnded = res.hasPrelaunchEnded;
+        this.formData.configurationContent = res.configurationContent;
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./css/EndPrelaunch.scss";
+</style>
