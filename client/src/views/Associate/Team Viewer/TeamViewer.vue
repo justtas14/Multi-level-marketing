f42fd9385e3ca4522a38eb126ed294c25ca54a3a
justas09 <justtas14@gmail.com>
2020-01-01 18:31:28 +0200

    done tests

---
+++ client/src/views/Associate/Team Viewer/TeamViewer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="admin-contentContainer">
+        <div class="card">
+            <div class="card-content">
+                <span class="card-title">Team Viewer</span>
+                <rootExplorer v-bind:path="'/api/associate/downline'"></rootExplorer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import rootExplorer from '../../../components/RootExplorer/rootExplorer.vue';
+
+
+export default {
+    name: 'TeamViewer',
+    props: [],
+    data() {
+        return {
+        };
+    },
+    components: {
+        rootExplorer,
+    },
+    methods: {
+    },
+    created() {
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    @import './css/TeamViewer.scss';
+</style>
---
+++ client/src/views/PageNotFound/PageNotFound.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="admin-contentContainer">
+        <div class="card">
+            <div class="card-content">
+                <span class="card-title notFound">404 Page Not Found</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// import {
+//     mapActions, mapState, mapGetters, mapMutations,
+// } from 'vuex';
+
+export default {
+    name: 'PageNotFound',
+    components: {
+    },
+    props: [],
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    mounted() {
+
+    },
+    computed: {
+    },
+    created() {
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    @import './css/PageNotFound.scss';
+</style>
